b6c4a376c6630322bce6dc1a842ed7a1c536fb3c
ketonBever0 <lacikurucz09@gmail.com>
2023-11-19 00:27:41 +0100

    palette & mui/base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import { Button } from "@mui/base";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-b from-indigo_dye-400 to-80% to-indigo_dye-100">
+      <Button className="">Hello</Button>
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,82 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    colors: {
+      indigo_dye: {
+        DEFAULT: "#124e78",
+        100: "#040f18",
+        200: "#071f30",
+        300: "#0b2e48",
+        400: "#0e3e60",
+        500: "#124e78",
+        600: "#1c77b9",
+        700: "#3d9de2",
+        800: "#7dbeec",
+        900: "#bedef5",
+      },
+      platinum: {
+        DEFAULT: "#e6e6e9",
+        100: "#2c2c31",
+        200: "#575762",
+        300: "#848492",
+        400: "#b5b5be",
+        500: "#e6e6e9",
+        600: "#ececee",
+        700: "#f0f0f2",
+        800: "#f5f5f6",
+        900: "#fafafb",
+      },
+      sea_green: {
+        DEFAULT: "#5e8c61",
+        100: "#131c13",
+        200: "#263827",
+        300: "#38543a",
+        400: "#4b714e",
+        500: "#5e8c61",
+        600: "#7ba77e",
+        700: "#9cbd9e",
+        800: "#bdd3be",
+        900: "#dee9df",
+      },
+      saffron: {
+        DEFAULT: "#f9c22e",
+        100: "#392b02",
+        200: "#735504",
+        300: "#ac8005",
+        400: "#e6aa07",
+        500: "#f9c22e",
+        600: "#facf59",
+        700: "#fbdb82",
+        800: "#fce7ac",
+        900: "#fef3d5",
+      },
+      chili_red: {
+        DEFAULT: "#e03616",
+        100: "#2d0b04",
+        200: "#591609",
+        300: "#86210d",
+        400: "#b22c12",
+        500: "#e03616",
+        600: "#ec5b3e",
+        700: "#f1846e",
+        800: "#f5ad9e",
+        900: "#fad6cf",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
