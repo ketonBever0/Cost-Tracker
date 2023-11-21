da332ae96cdda7b19b2e5296e74f5eb48a5ff659
Kurucz Laci <lacikurucz09@gmail.com>
2023-11-21 23:57:50 +0100

    register continue

--- src/app/api/user/signup/route.ts
+++ src/app/api/user/signup/route.ts
@@ -1,12 +1,35 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/api/prisma";
+import { z } from "zod";
+import * as argon from "argon2";
 
-export async function POST(req:NextRequest) {
+const createUserSchema = z.object({
+  username: z.string().min(4).max(16),
+  password: z.string().min(8),
+});
 
-    const user = req.body;
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const validation = createUserSchema.safeParse(req.body);
 
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
 
-    
+  const hash = await argon.hash(body.password);
 
-
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: body.username,
+        password: hash,
+      },
+    });
+  } catch (error: any) {
+    if (error.meta.target.includes("key")) {
+      if (error.code == "P2002") {
+        return NextResponse.json("Felhasználónév foglalt!", { status: 403 });
+      }
+    }
+  }
 }
